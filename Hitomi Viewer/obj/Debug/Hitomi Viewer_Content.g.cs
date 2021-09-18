f6018206123f70d1c7bf9348780030b0ab7e189c
bass9030 <bass90301.naver.comm@gmail.com>
2021-09-18 09:34:06 +0900

    이미지 링크 구하는 방식 변경

--- Hitomi Viewer/obj/Debug/Hitomi Viewer_Content.g.cs
+++ Hitomi Viewer/obj/Debug/Hitomi Viewer_Content.g.cs
@@ -8,6 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("avifdec.exe")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("libwebp.dll")]
 
 
